52e315d341ec2c1820eeae12d2eaea7d83f28ed2
Joao Assef <joaoassef@gmail.com>
2025-06-02 13:36:26 -0300

    API KEY

--- agendamento/src/useCheckCadastro.js
+++ agendamento/src/useCheckCadastro.js
@@ -1,4 +1,4 @@
-// useCheckCadastro.js
+// "use client";
 import { useState, useEffect } from 'react';
 
 export function useCheckCadastro() {
@@ -7,11 +7,23 @@ export function useCheckCadastro() {
   useEffect(() => {
     const check = async () => {
       try {
-        const response = await fetch('http://localhost:5025/api/Administrador');
-        if (!response.ok) throw new Error();
+        const response = await fetch('http://localhost:5236/api/Administrador', {
+          method: 'GET', // Or the appropriate HTTP method for your API
+          headers: {
+            'x-api-key': 'fwjfpjewfokwfwqww65fdqw4fwe4veew41f5e6fw65c1wec56e1ve56qf6ewfe1f'
+          },
+        });
+
+        if (!response.ok) {
+          // You might want to handle different HTTP status codes here
+          // For example, if response.status === 401, it's an authentication error
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
         const data = await response.json();
         setStatus(data.length > 0 ? 1 : 0);
-      } catch {
+      } catch (error) {
+        console.error("Failed to fetch administrator status:", error);
         setStatus(0);
       }
     };
