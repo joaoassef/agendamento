7d7e5b6c94dab9f9bb14cf29e3df2bde0f928fea
Joao Assef <joaoassef@gmail.com>
2025-06-27 10:37:01 -0300

    teste

--- agendamento/next.config.js
+++ agendamento/next.config.js
@@ -1,6 +1,24 @@
-require('dotenv').config(); // carrega as variáveis do .env.local
+// require('dotenv').config(); // carrega as variáveis do .env.local
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5236';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5236';
+
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: `${API_URL}/api/:path*`,
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+// DENTRO do container do site, o serviço da API se chama "api"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://api:8080';
 
 const nextConfig = {
   async rewrites() {
@@ -14,3 +32,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
